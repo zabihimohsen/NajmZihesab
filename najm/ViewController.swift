4cf1868eae1803115a5163ab121e57bd074a226e
zabihimohsen <mohsen.zabihi10@gmail.com>
2018-11-22 16:48:48 +0330

    delete some extra that was added for test

--- najm/ViewController.swift
+++ najm/ViewController.swift
@@ -12,7 +12,6 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        var x = 2 ;
         // Do any additional setup after loading the view, typically from a nib.
     }
 
