40af5ce9b21a5935a12099ef05a10a6010b4a5c7
zabihimohsen <zabihimohsen@rocketmail.com>
2018-11-22 14:23:08 +0330

    Initial Commit

---
+++ najm/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  najm
+//
+//  Created by Mohsen on 11/22/18.
+//  Copyright © 2018 virafanavar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
